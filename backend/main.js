03ee7664c817d487ebb8bfca47851acd700ddae9
valtteriviirret <viirretvaltteri@gmail.com>
2022-12-16 01:29:55 +0200

    initial commit

---
+++ backend/main.js
@@ -0,0 +1,126 @@
+const express = require('express');
+const process = require('process');
+const webSocket = require('ws');
+const path = require('path');
+const http = require('http');
+const fs = require('fs');
+
+const app = express();
+const port = 3000;
+const wsPort = 3001;
+
+app.use(express.static(path.resolve("../frontend/")));
+
+const httpServer = http.createServer(app).listen(wsPort);
+const ws = new webSocket.Server({server: httpServer});
+
+let bingos = {}
+let connectionsInBingo = {}
+
+function saveBingo(name) {
+    fs.writeFile(path.resolve("bingos/" + name + ".json"), JSON.stringify(bingos[name]), 
+    (err) => {
+        if(err)
+            console.log(err)
+    })
+}
+
+function findBingo(client) {
+    for (const [key, value] of Object.entries(connectionsInBingo)) {
+        for(let i = 0; i < value.length; i++) {
+            if(value[i] === client)
+                return key;
+        }
+    }
+
+    return undefined;
+}
+
+ws.on("connection", (client) => {
+    client.on("message", (payload) => {
+        let msg = undefined;
+
+        try {
+            msg = JSON.parse(payload.toString());
+            console.log(msg)
+        }
+
+        catch(e) {
+            client.close();
+            return
+        }
+
+        if(msg.cmd === "create") {
+            if(findBingo(client) !== undefined) {
+                console.log("Connection already in a bingo :-)")
+                client.close()
+                return
+            }
+
+            const length = 5
+            let result = '';
+            const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            const charactersLength = characters.length;
+
+            for(let i = 0; i < length; i++)
+                result += characters.charAt(Math.floor(Math.random() * charactersLength));
+
+            bingos[result] = msg.bingo
+            connectionsInBingo[result] = []
+            saveBingo(result)
+
+            msg = {
+                cmd: "load",
+                id: result
+            }
+        }
+
+        if(msg.cmd === "load") {
+            if(msg.id in bingos) {
+                msg.result = bingos[msg.id]
+                connectionsInBingo[msg.id].push(client)
+            }
+        }
+
+        else if(findBingo(client) === undefined) {
+            console.log("Connection not in a bingo :-(")
+            client.close()
+            return
+        }
+
+        if(msg.cmd === "cross") {
+            const bingo = findBingo(client)
+            bingos[bingo].data[msg.index].crossed = msg.shouldCross
+
+            connectionsInBingo[bingo].forEach((c) => {
+                c.send(JSON.stringify(msg))
+            })
+
+            saveBingo(bingo)
+            return
+        }
+
+        client.send(JSON.stringify(msg))
+    });
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("../frontend/index.html"));
+})
+
+app.listen(port, () => {
+    console.log(`The server is listening on port ${port}`);
+
+    if(!fs.existsSync("bingos"))
+        fs.mkdirSync("bingos")
+
+    const files = fs.readdirSync("bingos")
+
+    files.forEach((f) => {
+        const name = f.substr(0, f.lastIndexOf("."))
+        const data = fs.readFileSync(path.resolve("bingos/" + f))
+
+        bingos[name] = JSON.parse(data)
+        connectionsInBingo[name] = []
+    })
+})
---
+++ frontend/index.js
@@ -0,0 +1,139 @@
+'use strict';
+
+const ws = new WebSocket("ws://" + location.hostname + ":3001");
+
+function sendMessage(json) {
+    ws.send(JSON.stringify(json))
+}
+
+ws.addEventListener("open", () => {
+    const msg = {
+        "cmd": "load",
+        "id": location.hash.substring(1)
+    }
+
+    sendMessage(msg)
+})
+
+ws.addEventListener("message", (event) => {
+    const msg = JSON.parse(event.data.toString())
+
+    if(msg.cmd === "load") {
+        if(msg.result === undefined) {
+            let width = parseInt(prompt("Enter width"))
+            let height = parseInt(prompt("Enter height"))
+
+            generateBingoTable(width, height)
+            setupBingoTable(width, height)
+        }
+
+        else {
+            location.hash = msg.id
+
+            generateBingoTable(msg.result.width, msg.result.height)
+            fillBingoTable(msg.result.data)
+        }
+    }
+
+    else if(msg.cmd === "cross") {
+        const div = document.getElementById(msg.index.toString())
+        
+        if(msg.shouldCross)
+            div.classList.add("crossed");
+
+        else div.classList.remove("crossed");
+    }
+})
+
+function setupBingoTable(width, height) {
+    for(let i = 0; i < width * height; i++) {
+        const div = document.getElementById(i.toString())
+
+        div.onclick = (event) => {
+            const value = prompt("Enter text")
+            event.target.innerHTML = value
+        }
+    }
+
+    const submit = document.createElement("button")
+    submit.type = "button"
+    submit.innerHTML = "Create bingo"
+
+    submit.onclick = () => {
+        let data = []
+
+        for(let i = 0; i < width * height; i++) {
+            const div = document.getElementById(i.toString())
+            data.push({
+                text: div.innerHTML,
+                crossed: false
+            })
+        }
+
+        const msg = {
+            cmd: "create",
+            
+            bingo: {
+                width: width,
+                height: height,
+                data: data
+            }
+        }
+
+        document.getElementById("main").innerHTML = ""
+        sendMessage(msg)
+    }
+    
+
+    document.getElementById("main").appendChild(submit)
+}
+
+function fillBingoTable(data) {
+    for(let i = 0; i < data.length; i++) {
+        const entry = data[i]
+        const div = document.getElementById(i.toString())
+
+        div.innerHTML = entry.text
+
+        if(entry.crossed)
+            div.classList.add("crossed");
+
+        div.onclick = (event) => {
+            const shouldCross = !event.target.classList.contains("crossed")
+
+            const msg = {
+                cmd: "cross",
+                index: event.target.id,
+                shouldCross: shouldCross
+            }
+
+            sendMessage(msg)
+        }
+    }
+}
+
+function generateBingoTable(width, height) {
+    const table = document.createElement("table")
+    let i = 0
+
+    for(let y = 0; y < height; y++) {
+        const row = document.createElement("tr")
+
+        for(let x = 0; x < width; x++) {
+            const column = document.createElement("th")
+            const div = document.createElement("div")
+
+            div.className = "bingoentry"
+            div.id = i
+
+            column.appendChild(div)
+            row.appendChild(column)
+        
+            i++
+        }
+
+        table.appendChild(row)
+    }
+
+    document.getElementById("main").appendChild(table)
+}
